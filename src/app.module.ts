df64b92081ba809641459b6eec51124ffc09a129
ribbinpo <ribbinpo@gmail.com>
2023-11-28 13:58:34 +0700

    feat: :wrench: setup mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigsModule } from './configs/configs.module';
+import { DatabasesModule } from './databases/databases.module';
 
 @Module({
-  imports: [ConfigsModule],
+  imports: [ConfigsModule, DatabasesModule],
   controllers: [AppController],
   providers: [AppService],
 })
