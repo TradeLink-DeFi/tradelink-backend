00c889cf2cdeeabaaa4341f7e7ed3c68b55955e4
ribbinpo <ribbinpo@gmail.com>
2023-12-01 17:18:04 +0700

    feat: :zap: enable core

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   await app.listen(PORT, () =>
     console.log(`Server is running on port ${PORT}`),
   );
